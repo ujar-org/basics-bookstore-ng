dd92d05330b1f5f58a003e0998487ca36538adbf
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-05-26 12:36:03 +0300

    feat: create restful bookstore app template repository

---
+++ src/main/java/org/ujar/basics/restful/bookstore/config/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package org.ujar.basics.restful.bookstore.config;
+
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.ujar.boot.starter.logbook.LogbookJsonBodyFilter;
+import org.ujar.boot.starter.logbook.LogbookResponseOnStatus;
+
+@Configuration
+@LogbookResponseOnStatus
+@LogbookJsonBodyFilter
+@EnableJpaRepositories({"org.ujar.basics.restful.bookstore.repository"})
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class ApplicationConfig {
+
+  @Bean
+  public SpringLiquibase liquibase(@Autowired DataSource dataSource) {
+    var liquibase = new SpringLiquibase();
+    liquibase.setChangeLog("classpath:liquibase/master.xml");
+    liquibase.setDataSource(dataSource);
+    return liquibase;
+  }
+}
