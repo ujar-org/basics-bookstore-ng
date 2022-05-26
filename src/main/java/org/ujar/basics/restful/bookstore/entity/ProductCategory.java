7fc34e301cd632375b6fa658195e5398d6409435
Dima Denysenko <dmytro.denysenko@dataart.com>
2022-05-26 18:55:43 +0300

    feat: create REST endpoints for product & category resources

---
+++ src/main/java/org/ujar/basics/restful/bookstore/entity/ProductCategory.java
@@ -0,0 +1,42 @@
+package org.ujar.basics.restful.bookstore.entity;
+
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = ProductCategory.TABLE_NAME)
+@Getter
+@Setter
+public class ProductCategory {
+
+  protected static final String TABLE_NAME = "bookstore_categories";
+
+  @Id
+  @SequenceGenerator(
+      name = "product_category_id_seq",
+      sequenceName = "product_category_id_seq"
+  )
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "product_category_id_seq"
+  )
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+  private Set<Product> products;
+
+}
---
+++ src/main/java/org/ujar/basics/restful/bookstore/web/ProductCategoryController.java
@@ -0,0 +1,63 @@
+package org.ujar.basics.restful.bookstore.web;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ujar.basics.restful.bookstore.entity.ProductCategory;
+import org.ujar.basics.restful.bookstore.repository.ProductCategoryRepository;
+import org.ujar.boot.starter.restful.web.dto.ErrorResponse;
+
+@RestController
+@Tag(name = "Product category controller", description = "API for product categories management")
+@RequestMapping("/api/v1/product-categories")
+@Validated
+@RequiredArgsConstructor
+public class ProductCategoryController {
+
+  private final ProductCategoryRepository repository;
+
+  @GetMapping("/{id}")
+  @Operation(
+      description = "Retrieve product category by id.",
+      responses = {
+          @ApiResponse(responseCode = "200",
+                       description = "Success"),
+          @ApiResponse(responseCode = "500",
+                       description = "Internal error",
+                       content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+          @ApiResponse(responseCode = "400",
+                       description = "Bad request",
+                       content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      })
+  public ResponseEntity<ProductCategory> findById(@PathVariable Long id) {
+    return new ResponseEntity<>(repository.findById(id).orElse(null), HttpStatus.OK);
+  }
+
+  @GetMapping
+  @Operation(
+      description = "Retrieve product categories list.",
+      responses = {
+          @ApiResponse(responseCode = "200",
+                       description = "Success"),
+          @ApiResponse(responseCode = "500",
+                       description = "Internal error",
+                       content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+          @ApiResponse(responseCode = "400",
+                       description = "Bad request",
+                       content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      })
+  public ResponseEntity<List<ProductCategory>> findAll() {
+    return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+  }
+}
