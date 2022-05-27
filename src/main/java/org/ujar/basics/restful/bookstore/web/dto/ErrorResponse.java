5609ea19a55c96c48ccf62d47bc89ba10d87acae
Dima Denysenko <1253839+dimdnk@users.noreply.github.com>
2022-05-27 18:38:21 +0300

    fix: suppress findbugs warnings

--- src/main/java/org/ujar/basics/restful/bookstore/web/dto/ErrorResponse.java
+++ src/main/java/org/ujar/basics/restful/bookstore/web/dto/ErrorResponse.java
@@ -1,7 +1,9 @@
 package org.ujar.basics.restful.bookstore.web.dto;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.List;
 
+@SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
 public record ErrorResponse(List<Error> errors) {
 
   public static ErrorResponse singleError(Throwable throwable) {
