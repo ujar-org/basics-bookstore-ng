1abea7f31b15b6b3ad0855c8a674d7b5e513ec73
Dima Denysenko <coding@dimdnk.com>
2022-05-29 18:49:07 +0300

    feat: create REST endpoints for product & category resources (#1)
    * feat: create REST endpoints for product & category resources
    
    * feat: add default REST Resource error handler
    
    * refactor: error handling refinement
    
    * fix: suppress findbugs warnings
    
    * feat: use limit and offset params for pagination

--- src/main/java/org/ujar/basics/restful/bookstore/config/ApplicationConfig.java
+++ src/main/java/org/ujar/basics/restful/bookstore/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.ujar.boot.starter.logbook.LogbookJsonBodyFilter;
 import org.ujar.boot.starter.logbook.LogbookResponseOnStatus;
@@ -17,10 +18,11 @@ import org.ujar.boot.starter.logbook.LogbookResponseOnStatus;
 @EnableJpaRepositories({"org.ujar.basics.restful.bookstore.repository"})
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EnableSpringDataWebSupport
 public class ApplicationConfig {
 
   @Bean
-  public SpringLiquibase liquibase(@Autowired DataSource dataSource) {
+  SpringLiquibase liquibase(@Autowired DataSource dataSource) {
     var liquibase = new SpringLiquibase();
     liquibase.setChangeLog("classpath:liquibase/master.xml");
     liquibase.setDataSource(dataSource);
