d15d41c05c16d3d34882e9f15a761e2e7a9177e0
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-06-01 14:04:43 +0300

    feat: implement filter by name for /api/v1/products
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/test/java/org/ujar/basics/restful/bookstore/web/ProductControllerTest.java
+++ src/test/java/org/ujar/basics/restful/bookstore/web/ProductControllerTest.java
@@ -3,17 +3,23 @@ package org.ujar.basics.restful.bookstore.web;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
+import org.ujar.basics.restful.bookstore.entity.Product;
 import org.ujar.basics.restful.bookstore.repository.ProductRepository;
 
 @WebMvcTest(value = ProductController.class)
@@ -22,7 +28,7 @@ class ProductControllerTest {
   private final MockMvc mockMvc;
 
   @MockBean
-  private ProductRepository repository;
+  private ProductRepository productRepository;
 
   ProductControllerTest(@Autowired MockMvc mockMvc) {
     this.mockMvc = mockMvc;
@@ -30,17 +36,29 @@ class ProductControllerTest {
 
   @Test
   @SneakyThrows
-  void findByIdShouldReceiveNotFound() {
+  void findByIdShouldReturnNotFound() {
     mockMvc.perform(
             get("/api/v1/products/1"))
         .andExpect(status().isNotFound());
-
-    verify(repository, atLeastOnce()).findById(1L);
+    verify(productRepository, atLeastOnce()).findById(1L);
   }
 
   @Test
   @SneakyThrows
-  void findAllShouldReceiveEmptyList() {
+  void findAllByNameShouldReturnOneLinuxBook() {
+    var pageRequest = PageRequest.of(0, 10);
+    when(productRepository.findByNameContaining("Linux", pageRequest))
+        .thenReturn(new PageImpl<>(List.of(new Product())));
+    mockMvc.perform(
+            get("/api/v1/products").param("name", "Linux"))
+        .andDo(print())
+        .andExpect(status().isOk());
+    verify(productRepository).findByNameContaining("Linux", pageRequest);
+  }
+
+  @Test
+  @SneakyThrows
+  void findAllShouldReturnEmptyList() {
     mockMvc.perform(
             get("/api/v1/products")
                 .param("page", "1")
@@ -48,7 +66,7 @@ class ProductControllerTest {
         .andExpect(status().isOk());
 
     var captor = ArgumentCaptor.forClass(Pageable.class);
-    verify(repository).findAll(captor.capture());
+    verify(productRepository).findAll(captor.capture());
     assertThat(captor.getValue().getPageNumber()).isEqualTo(1L);
   }
 }
