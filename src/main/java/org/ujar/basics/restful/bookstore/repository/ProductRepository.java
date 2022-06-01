2e947d48e2366ec4ee663759666cc7a4f1122c09
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-06-01 10:20:48 +0300

    feat: create /api/v1/product-categories/{id}/products endpoint
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/basics/restful/bookstore/repository/ProductRepository.java
+++ src/main/java/org/ujar/basics/restful/bookstore/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package org.ujar.basics.restful.bookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.ujar.basics.restful.bookstore.entity.Product;
+import org.ujar.basics.restful.bookstore.entity.ProductCategory;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>, JpaRepository<Product, Long> {
+  Page<Product> findAllByCategory(ProductCategory category, Pageable pageable);
 }
--- src/test/java/org/ujar/basics/restful/bookstore/web/ProductCategoryControllerTest.java
+++ src/test/java/org/ujar/basics/restful/bookstore/web/ProductCategoryControllerTest.java
@@ -2,18 +2,27 @@ package org.ujar.basics.restful.bookstore.web;
 
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.ujar.basics.restful.bookstore.entity.Product;
+import org.ujar.basics.restful.bookstore.entity.ProductCategory;
 import org.ujar.basics.restful.bookstore.repository.ProductCategoryRepository;
+import org.ujar.basics.restful.bookstore.repository.ProductRepository;
 
 @WebMvcTest(value = ProductCategoryController.class)
 class ProductCategoryControllerTest {
@@ -21,7 +30,10 @@ class ProductCategoryControllerTest {
   private final MockMvc mockMvc;
 
   @MockBean
-  private ProductCategoryRepository repository;
+  private ProductCategoryRepository categoryRepository;
+
+  @MockBean
+  private ProductRepository productRepository;
 
   public ProductCategoryControllerTest(@Autowired MockMvc mockMvc) {
     this.mockMvc = mockMvc;
@@ -32,9 +44,67 @@ class ProductCategoryControllerTest {
   void findByIdShouldReceiveNotFound() {
     mockMvc.perform(
             get("/api/v1/product-categories/1"))
-        .andDo(print())
         .andExpect(status().isNotFound());
-    verify(repository, atLeastOnce()).findById(1L);
+    verify(categoryRepository, atLeastOnce()).findById(1L);
+  }
+
+  @Test
+  @SneakyThrows
+  void findProductsByCategoryIdShouldReceiveNotFound() {
+    mockMvc.perform(
+            get("/api/v1/product-categories/1/products"))
+        .andExpect(status().isNotFound());
+    verify(categoryRepository, atLeastOnce()).findById(1L);
+  }
+
+  @Test
+  @SneakyThrows
+  void findProductsByCategoryIdShouldReceiveListWithOneBook() {
+    var category =  new ProductCategory(1L, "Books", new HashSet<Product>());
+    var pageRequest = PageRequest.of(0, 10);
+    when(categoryRepository.findById(1L)).thenAnswer(
+        (inv) -> Optional.of(category)
+    );
+    when(productRepository.findAllByCategory(category, pageRequest))
+        .thenReturn(new PageImpl<>(List.of(new Product())));
+    mockMvc.perform(
+            get("/api/v1/product-categories/1/products"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().json(
+            """
+                {
+                  "content":[
+                    {
+                      "id":null,
+                      "category":null,
+                      "sku":null,
+                      "name":null,
+                      "description":null,
+                      "unitPrice":null,
+                      "imageUrl":null,
+                      "active":false,
+                      "unitsInStock":0
+                    }
+                  ],
+                  "pageable":"INSTANCE",
+                  "totalPages":1,
+                  "totalElements":1,
+                  "last":true,
+                  "size":1,
+                  "number":0,
+                  "sort":{
+                    "empty":true,
+                    "sorted":false,
+                    "unsorted":true
+                  },
+                  "numberOfElements":1,
+                  "first":true,
+                  "empty":false
+                }""", true
+        ));
+    verify(categoryRepository).findById(1L);
+    verify(productRepository).findAllByCategory(category, pageRequest);
   }
 
   @Test
@@ -42,11 +112,10 @@ class ProductCategoryControllerTest {
   void findAllShouldReceiveEmptyList() {
     mockMvc.perform(
             get("/api/v1/product-categories"))
-        .andDo(print())
         .andExpect(status().isOk())
         .andExpect(content().json(
             "[]", true
         ));
-    verify(repository).findAll();
+    verify(categoryRepository).findAll();
   }
 }
