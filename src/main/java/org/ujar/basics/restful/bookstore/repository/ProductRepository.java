5611841c1e4e4141aca1a8b137f4649a874c445b
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-06-01 14:04:43 +0300

    feat: implement filter by name for /api/v1/products

--- src/main/java/org/ujar/basics/restful/bookstore/repository/ProductRepository.java
+++ src/main/java/org/ujar/basics/restful/bookstore/repository/ProductRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.ujar.basics.restful.bookstore.entity.Product;
 import org.ujar.basics.restful.bookstore.entity.ProductCategory;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>, JpaRepository<Product, Long> {
+  @Transactional(readOnly = true)
   Page<Product> findAllByCategory(ProductCategory category, Pageable pageable);
+
+  @Transactional(readOnly = true)
+  Page<Product> findByNameContaining(String name, Pageable pageable);
 }
