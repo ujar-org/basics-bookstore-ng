98aebc9807539ab2f9d18fa4c3c935edf945acf3
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-08-04 11:59:35 +0300

    feat: improve config classes encapsulation
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/basics/restful/bookstore/config/LocalWebMvcConfig.java
+++ src/main/java/org/ujar/basics/restful/bookstore/config/LocalWebMvcConfig.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Profile("local")
-public class LocalWebMvcConfig implements WebMvcConfigurer {
+class LocalWebMvcConfig implements WebMvcConfigurer {
   @Override
-  public void addCorsMappings(CorsRegistry registry) {
+  void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:8080", "http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
