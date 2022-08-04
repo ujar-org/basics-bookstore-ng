d38e21eff3af6670fa8d9a17aa0db4c80b597e50
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-08-04 20:28:50 +0300

    fix: method access level

--- src/main/java/org/ujar/basics/restful/bookstore/config/LocalWebMvcConfig.java
+++ src/main/java/org/ujar/basics/restful/bookstore/config/LocalWebMvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("local")
 class LocalWebMvcConfig implements WebMvcConfigurer {
   @Override
-  void addCorsMappings(CorsRegistry registry) {
+  public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:8080", "http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
