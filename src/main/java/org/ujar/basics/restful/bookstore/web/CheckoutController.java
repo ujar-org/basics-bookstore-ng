afbe13d116d66c3a552ed0d514aac5eb47af61c1
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2022-08-09 12:06:19 +0300

    feat: use records as controllers

--- src/main/java/org/ujar/basics/restful/bookstore/web/CheckoutController.java
+++ src/main/java/org/ujar/basics/restful/bookstore/web/CheckoutController.java
@@ -1,6 +1,5 @@
 package org.ujar.basics.restful.bookstore.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +10,7 @@ import org.ujar.basics.restful.bookstore.web.dto.PurchaseResponse;
 
 @RestController
 @RequestMapping("/api/v1/checkout")
-@RequiredArgsConstructor
-class CheckoutController {
-
-  private final CheckoutService checkoutService;
+record CheckoutController(CheckoutService checkoutService) {
 
   @PostMapping("/purchase")
   PurchaseResponse placeOrder(@RequestBody final Purchase purchase) {
