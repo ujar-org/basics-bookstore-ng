86bd369b88daa46d7a00dd1661795c0437c9732e
Dima Denysenko <coding@dimdnk.com>
2023-06-03 18:34:04 +0300

    chore: enable reasonable defaults for observability components

--- src/main/java/org/ujar/bookstore/config/ApplicationConfig.java
+++ src/main/java/org/ujar/bookstore/config/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package org.ujar.bookstore.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaAuditing
 @EnableTransactionManagement
 @EnableSpringDataWebSupport
+@OpenAPIDefinition(info = @Info(title = "Bookstore API", version = "23.0.0"))
 class ApplicationConfig {
 
 }
