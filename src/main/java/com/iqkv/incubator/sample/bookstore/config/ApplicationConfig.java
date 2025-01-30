499ac7b28c21e64d94e064bf8a2a77aaedf106d4
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-01-30 19:09:56 +0200

    chore: update streamline version and copyright headers

--- src/main/java/com/iqkv/incubator/sample/bookstore/config/ApplicationConfig.java
+++ src/main/java/com/iqkv/incubator/sample/bookstore/config/ApplicationConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 IQKV Team.
+ * Copyright 2025 IQKV Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaAuditing
 @EnableTransactionManagement
 @EnableSpringDataWebSupport
-@OpenAPIDefinition(info = @Info(title = "Bookstore API", version = "24.0.0"))
+@OpenAPIDefinition(info = @Info(title = "Bookstore API", version = "25.0.0"))
 class ApplicationConfig {
 
 }
